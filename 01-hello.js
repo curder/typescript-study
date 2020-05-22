d0e0744750086cde82a41c3037c5b7c1e14e493f
curder <curder@foxmail.com>
2020-05-22 19:54:21 +0800

    init project

---
+++ 01-hello.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.Hello = void 0;
+var Hello = /** @class */ (function () {
+    function Hello() {
+    }
+    return Hello;
+}());
+exports.Hello = Hello;
