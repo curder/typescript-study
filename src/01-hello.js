4857d42997d17946d76f1d9c521d195c481ae3b7
curder <curder@foxmail.com>
2020-05-23 01:21:09 +0800

    delete *.js files

--- src/01-hello.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Hello = void 0;
-var Hello = /** @class */ (function () {
-    function Hello() {
-    }
-    return Hello;
-}());
-exports.Hello = Hello;
--- src/14-module/two.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// 从 one 模块中导入 prop1属性，func1方法和Class1类
-var one_1 = require("./one");
-console.log(one_1.prop1);
-console.log(one_1.func1);
-console.log(one_1.Class1);
