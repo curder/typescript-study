d3fcbe57aa4759c950f70853f42646b635a1ddac
curder <q.curder@gmail.com>
2025-04-02 13:38:04 +0800

    feat(index.ts): Add DOM manipulation to display message
    The commit adds code to create an h1 element, set its innerHTML to the 'hello' message, and append it to the body. This was done to enhance the visual display of the 'hello' message in the application.

--- src/07-modules/webpack/src/index.ts
+++ src/07-modules/webpack/src/index.ts
@@ -4,4 +4,6 @@ const message = "hello";
 
 console.log(message, message.length, sum(1, 2));
 
-export {};
+const h1Element = document.createElement("h1");
+h1Element.innerHTML = message;
+document.body.appendChild(h1Element);
